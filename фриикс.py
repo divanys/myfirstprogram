7f7fa4eff946e7f58c02e3fd5c37d1944119873e
divanys <92179316+divanys@users.noreply.github.com>
2022-07-07 16:51:48 +0300

    Add files via upload

---
+++ фриикс.py
@@ -0,0 +1,27 @@
+# #мы вводим строку, выводится символ юникода каждого символа
+# i = list(map(str, input()))
+# for k in range(len(i)):
+#     print(k, ord(i[k]))
+
+# i = getattr('dss', 'trtr', ' yyyyyyy')
+# print(i)
+
+
+# Создано 29.06.2022(для тех, кто шарит)
+# Я НЕ РАСИСТ И НЕ ГОМОФОБ, я просто ржу)
+class YourFamily:
+    dad = 'присутствует'
+    mom = 'шшшшшшшлюха'
+    brother1 = '.!.'
+    brother2 = '.!.'
+    brother3 = '.!.'
+    brother4 = '.!.'
+    brother5 = '.!.'
+    brother6 = '.!.'
+    you = '       n\n      nnn\n    nnnnn'
+your = YourFamily()
+print('Подсчитаем твоих отчимов')
+for i in range(len('твои отчимы')):
+    print(i+1, your.dad)
+if len('твои отчимы') == 11:
+    print('AAAAAAAAAAAAAAAAAAAAAAAAA\n     НИХУЯ У ТЯ ГАЧИ\nAAAAAAAAAAAAAAAAAAAAAAAAA')
